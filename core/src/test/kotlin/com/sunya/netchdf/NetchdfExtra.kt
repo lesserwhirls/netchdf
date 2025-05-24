5114fb94ef0a7fa3ea0adb7be2dedd04bdd049b7
JohnLCaron <jcaron1129@gmail.com>
2025-05-24 12:06:34 -0600

    Fix typos in docs

--- core/src/test/kotlin/com/sunya/netchdf/NetchdfExtra.kt
+++ core/src/test/kotlin/com/sunya/netchdf/NetchdfExtra.kt
@@ -24,7 +24,10 @@ class NetchdfExtra {
         @AfterAll
         fun afterAll() {
             if (versions.size > 0) {
-                versions.keys.forEach{ println("$it = ${versions[it]!!.size } files") }
+                val sversions = versions.toSortedMap()
+                sversions.keys.forEach{ println("$it = ${sversions[it]!!.size } files") }
+                val total = sversions.keys.map{ sversions[it]!!.size }.sum()
+                println("total # files = $total")
             }
             Stats.show()
         }
